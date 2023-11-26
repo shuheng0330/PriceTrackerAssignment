793f30744af89a0767ef6379afeda713266a4e5f
shuheng0330 <thongshuheng0330@gmail.com>
2023-11-27 00:13:10 +0800

    Initial commit

---
+++ src/main/java/com/example/javafxloginvideo/DBUtils.java
@@ -0,0 +1,44 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DBUtils {
+
+    public static void changeScene(ActionEvent event, String fxmlFile, String title,String username){
+        Parent root=null;
+
+        if(username != null){
+            try {
+                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+                root =loader.load();
+                LoggedInController loggedInController =loader.getController();
+                loggedInController.setUserInformation(username);
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }else {
+            try{
+                root=FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    public static void signUpUser(ActionEvent event,String username,String password,String email,int contact_no ){
+         
+
+    }
+}
